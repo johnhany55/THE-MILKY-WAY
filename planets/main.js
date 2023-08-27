5f680bc11b0c09424b8ce55776588f991815133c
John Hany <127802698+johnhany55@users.noreply.github.com>
2023-08-27 16:41:20 +0300

    Add files via upload

---
+++ planets/main.js
@@ -0,0 +1,52 @@
+let detailsh = document.getElementById('data_name');
+let detailsp = document.getElementById('data_pr');
+let nameh1 = document.getElementById('nameofplanet');
+let imgofplanet = document.getElementById('imgofplanet')
+function mercury(){
+    imgofplanet.src='img/3D_Mercury.png'
+    nameh1.innerHTML = 'Mercury';
+    detailsh.innerHTML = 'Mercury';
+    detailsp.innerHTML = 'Diameter:	4,879 km <br>Mass:	3.29 × 10^23 kg (0.06 Earths) <br>Moons:	None <br>Orbit Distance:	57,909,227 km (0.39 AU) <br>Orbit Period:	88 days <br>Surface Temperature:	-173 to 427°C <br> First Record:	14th century BC';
+}
+function venus(){
+    imgofplanet.src='img/3D_Venus.png'
+    nameh1.innerHTML = 'Venus';
+    detailsh.innerHTML = 'Venus';
+    detailsp.innerHTML = 'Diameter:	12,104 km <br>Mass:	4.87 × 10^24 kg (0.82 Earths) <br>Moons:	None<br>Orbit Distance:	108,209,475 km (0.73 AU)<br>Orbit Period:	225 days<br>Surface Temperature:	462 °C<br>First Record:	17th century BC';
+}
+function earth(){
+    imgofplanet.src='img/The_Earth.png'
+    nameh1.innerHTML = 'Earth';
+    detailsh.innerHTML = 'Earth';
+    detailsp.innerHTML = 'Equatorial Diameter:	12,756 km<br>Polar Diameter:	12,714 km<br>Mass:	5.97 × 10^24 kg<br>Moons:	1 (The Moon)<br>Orbit Distance:	149,598,262 km (1 AU)<br>Orbit Period:	365.24 days<br>Surface Temperature:	-88 to 58°C';
+}
+function mars(){
+    imgofplanet.src='img/3D_Mars.png'
+    nameh1.innerHTML = 'Mars';
+    detailsh.innerHTML = 'Mars';
+    detailsp.innerHTML = 'Polar Diameter:	6,752 km<br>Mass:	6.39 × 10^23 kg (0.11 Earths) <br>Moons:	2 (Phobos & Deimos) <br>Orbit Distance:	227,943,824 km (1.38 AU) <br>Orbit Period:	687 days (1.9 years)<br>Surface Temperature:	-87 to -5 °C<br>First Record:	2nd millennium BC';
+}
+function jupiter(){
+    imgofplanet.src='img/3D_Jupiter.png'
+    nameh1.innerHTML = 'Jupiter';
+    detailsh.innerHTML = 'Jupite';
+    detailsp.innerHTML = 'Polar Diameter:	133,709 km<br>Mass:	1.90 × 10^27 kg (318 Earths)<br>Moons:	79 (Io, Europa, Ganymede & Callisto)<br>Rings:	4<br>Orbit Distance:	778,340,821 km (5.20 AU)<br>Orbit Period:	4,333 days (11.9 years)<br>Surface Temperature:	-108°C<br>First Record:	7th or 8th century BC';
+}
+function saturn(){
+    imgofplanet.src='img/3D_Saturn.png'
+    nameh1.innerHTML = 'Saturn';
+    detailsh.innerHTML = 'Saturn';
+    detailsp.innerHTML = 'Polar Diameter:	108,728 km<br>Mass:	5.68 × 10^26 kg (95 Earths)<br>Moons:	82 (Titan, Enceladus, Iapetus & Rhea)<br>Rings:	30+ (7 Groups)<br>Orbit Distance:	1,426,666,422 km (9.58 AU)<br>Orbit Period:	10,756 days (29.5 years)<br>Surface Temperature:	-139 °C<br>First Record:	8th century BC';
+}
+function uranus(){
+    imgofplanet.src='img/3D_Uranus.png'
+    nameh1.innerHTML = 'Uranus';
+    detailsh.innerHTML = 'Uranus';
+    detailsp.innerHTML = 'Polar Diameter:	49,946 km<br>Mass:	8.68 × 10^25 kg (15 Earths)<br>Moons:	27 (Miranda, Titania, Ariel, Umbriel & Oberon)<br>Rings:	13<br>Orbit Distance:	2,870,658,186 km (19.22 AU)<br>Orbit Period:	30,687 days (84.0 years)<br>Surface Temperature:	-197 °C<br>Discovery Date:	March 13th 1781<br>';
+}
+function neptune(){
+    imgofplanet.src='img/3D_Neptune.png'
+    nameh1.innerHTML = 'Neptune';
+    detailsh.innerHTML = 'Neptune';
+    detailsp.innerHTML = 'Polar Diameter:	48,682 km<br>Mass:	1.02 × 10^26 kg (17 Earths)<br>Moons:	14 (Triton)<br>Rings:	5<br>Orbit Distance:	4,498,396,441 km (30.10 AU)<br>Orbit Period:	60,190 days (164.8 years)<br>Surface Temperature:	-201 °C<br>Discovery Date:	September 23rd 1846';
+}
